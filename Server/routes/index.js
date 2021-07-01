c1a22e34c161303cbfe93f72654e610e6e87ff7a
William Wolseley-Charles <williamwolseleycharles@gmail.com>
2021-07-01 20:22:21 +0100

    App Route

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/app', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
